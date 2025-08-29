de6f9cea2619354cdc9cfd23706cdd470daa3103
cutejiuge <cutejiuge@163.com>
2025-08-29 23:37:00 +0800

    20250829日题练习

---
+++ java-version/src/main/java/com/github/leetcode/LC3021/Solution.java
@@ -0,0 +1,13 @@
+package com.github.leetcode.LC3021;
+
+/**
+ * lc3021, 脑筋急转弯
+ *
+ * @author cutejiuge
+ * @since 2025/8/29 下午11:22
+ */
+public class Solution {
+    public long flowerGame(int n, int m) {
+        return (long) m * n / 2;
+    }
+}
